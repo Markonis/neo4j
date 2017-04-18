2ec642a3b8b68a9fdf1fe53f11795f1ee06ca378
Marko <contact.pavlovic@gmail.com>
2017-04-18 14:04:55 +0000

    Export service in index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var Session = require('./lib/session.js');
 var serializer = require('./lib/serializer.js');
 var cypherBuilder = require('./lib/cypher-builder.js');
 var Schema = require('./lib/schema.js');
+var service = require('./lib/service.js');
 
 module.exports = {
   Session: Session,
   serializer: serializer,
   cypherBuilder: cypherBuilder,
-  Schema: Schema
+  Schema: Schema,
+  service: service
 };
